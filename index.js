65a623a2b58b669807cededa85d7ad559552a480
Devansh Kansagra <22it423@bvmengineering.ac.in>
2024-05-03 12:43:16 +0530

    Added basic routes of the backend

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ require('./database/connect');
 
 
 // Basic Routes
+app.post('/signup', (req, res) => {
+    res.send('Signup');
+})
+app.post('/login', (req, res) => {
+    res.send('login');
+})
+app.get('/profile', (req, res) => {
+    res.send('profile');
+})
+app.delete('/logout', (req, res) => {
+    res.send('login');
+})
 
 app.listen(port, () => {
     console.log('Server is started at port ', port);
